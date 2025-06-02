4558a1ea9b42aaae714641b0e0deeffa1ecb9c0f
LordMorgana <morganbleasdale234@gmail.com>
2025-06-02 21:54:42 +0100

    markdown not on user message

--- src/components/Chat/Message.js
+++ src/components/Chat/Message.js
@@ -8,7 +8,12 @@ function Message ({role, children}) {
     return (
         <div className={styles.MessageContainer} id={styles[role]}>
             <div className={styles.Message}>
-                <Markdown className={styles.MessageContent}>{children}</Markdown>
+                {role === "model" && (
+                    <Markdown className={styles.MessageContent}>{children}</Markdown>
+                )}
+                {role === "user" && (
+                    <p className={styles.MessageContent}>{children}</p>
+                )}
             </div>
         </div>
     )
